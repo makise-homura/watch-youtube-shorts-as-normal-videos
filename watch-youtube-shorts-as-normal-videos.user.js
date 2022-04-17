b496c86597b9d0a3719fbb8c04750ef147ca12af
makise-homura <akemi_homura@kurisa.ch>
2022-04-17 22:49:01 +0300

    Work correctly not just when opening in new tab, but also in current one

--- watch-youtube-shorts-as-normal-videos.user.js
+++ watch-youtube-shorts-as-normal-videos.user.js
@@ -1,9 +1,48 @@
 // ==UserScript==
 // @name     Watch Youtube shorts as normal videos
-// @version  1
+// @version  2
 // @grant    none
 // @author   makise-homura
-// @include  https://www.youtube.com/shorts/*
+// @include  https://www.youtube.com/*
 // ==/UserScript==
 
-window.location.replace(window.location.toString().replace('/shorts/','/watch?v='))
+function replaceLocation()
+{
+    if (window.location.toString().includes('/shorts/'))
+    {
+        window.location.replace(window.location.toString().replace('/shorts/','/watch?v='))
+    }
+}
+
+// In case we opened a new page
+replaceLocation();
+
+// See https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes
+var oldHref = document.location.href;
+
+window.onload = function()
+{
+    var bodyList = document.querySelector("body")
+
+    var observer = new MutationObserver(function(mutations)
+    {
+        mutations.forEach(function(mutation)
+        {
+            if (oldHref != window.location.toString())
+            {
+                oldHref = window.location.toString();
+
+                // In case we just changed an URL of existing page
+                replaceLocation();
+            }
+        });
+    });
+
+    var config =
+    {
+        childList: true,
+        subtree: true
+    };
+
+    observer.observe(bodyList, config);
+};
